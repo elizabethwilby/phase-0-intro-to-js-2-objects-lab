4d408edbda31efe616d38ee74359688f3c3d29a6
elizabethwilby <elizabethwilby@Elizabeths-MacBook-Pro.local>
2026-05-06 12:22:36 -0400

    finished intro to js 2 objects lab

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Write your solution in this file!
+const employee = {
+  name: 'Sam',
+  streetAddress: '11 Broadway'
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+
+  const updatedEmployee = {
+    ...employee,
+    [key]: value
+  };
+
+  console.log(updatedEmployee);
+
+  return updatedEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+
+  employee[key] = value;
+
+  console.log(employee);
+
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+
+  const newEmployee = {
+    ...employee
+  };
+
+  delete newEmployee[key];
+
+  console.log(newEmployee);
+
+  return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+
+  delete employee[key];
+
+  console.log(employee);
+
+  return employee;
+}
